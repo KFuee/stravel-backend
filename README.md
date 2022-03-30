319166936277d98f1b1a1a6a23324588726671b1
KFuee <61166056+KFuee@users.noreply.github.com>
2022-03-30 22:35:01 +0200

    Configuración inicial

---
+++ README.md
@@ -0,0 +1,2 @@
+# urban-mobility-backend
+Backend de la guía turística UrbanMobility 
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (_req, res) => res.send('Hello World!'));
+
+app.listen(port, () =>
+  console.log(`UrbanMobility backend escuchando en el puerto ${port}!`)
+);
