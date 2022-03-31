d837bf8373abe185abce955338998e8a14e5504c
KFuee <jbisazac@icloud.com>
2022-03-31 20:36:43 +0200

    Configuración de logging http con morgan

---
+++ src/config/morgan.js
@@ -0,0 +1,25 @@
+const morgan = require('morgan');
+
+const config = require('./index');
+const logger = require('./logger');
+
+morgan.token('message', (_req, res) => res.locals.errorMessage || '');
+
+const ipFormat = () => (config.env === 'production' ? ':remote-addr - ' : '');
+
+const successFormat = `$${ipFormat()}:method :url :status - :response-time ms`;
+const successHandler = morgan(successFormat, {
+  skip: (_req, res) => res.statusCode >= 400,
+  stream: { write: (message) => logger.info(message.trim()) },
+});
+
+const errorFormat = `${ipFormat()}:method :url :status - :response-time ms - :message`;
+const errorHandler = morgan(errorFormat, {
+  skip: (_req, res) => res.statusCode < 400,
+  stream: { write: (message) => logger.error(message.trim()) },
+});
+
+module.exports = {
+  successHandler,
+  errorHandler,
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+const morgan = require('./config/morgan');
 const logger = require('./config/logger');
 
 const app = express();
 const port = 3000;
+
+// use morgan
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
 
 app.get('/', (_req, res) => res.send('Hello World!'));
 
