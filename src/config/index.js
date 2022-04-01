63acbde365314213ffd297d913505b6d0eb7afea
KFuee <jbisazac@icloud.com>
2022-04-01 13:01:09 +0200

    Conexión a la base de datos mongodb

--- src/config/index.js
+++ src/config/index.js
@@ -6,6 +6,13 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
+  mongoose: {
+    uri: process.env.MONGODB_URI,
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
 };
 
 module.exports = config;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+const mongoose = require('mongoose');
+
 const app = require('./config/express');
-const { port } = require('./config');
+const { port, mongoose: mongooseConfig } = require('./config');
 const logger = require('./config/logger');
 
-app.listen(port, () => logger.info(`Escuchando en el puerto ${port}`));
+mongoose.connect(mongooseConfig.uri, mongooseConfig.options, (err) => {
+  if (err) {
+    logger.error(err);
+    process.exit(1);
+  }
+
+  logger.info('Conectado a MongoDB');
+  app.listen(port, () => logger.info(`Escuchando en el puerto ${port}`));
+});
