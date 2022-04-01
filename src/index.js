df73188ef5fa4c6ba2157c2336389cbb32a72e5a
KFuee <jbisazac@icloud.com>
2022-04-01 13:12:29 +0200

    Manejo de errores inesperados

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ const app = require('./config/express');
 const { port, mongoose: mongooseConfig } = require('./config');
 const logger = require('./config/logger');
 
-mongoose.connect(mongooseConfig.uri, mongooseConfig.options, (err) => {
-  if (err) {
-    logger.error(err);
-    process.exit(1);
+let server;
+mongoose.connect(mongooseConfig.uri, mongooseConfig.options).then(() => {
+  logger.info('Conectado a MongoDB');
+  server = app.listen(port, () =>
+    logger.info(`Escuchando en el puerto ${port}`)
+  );
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Cerrando el servidor Express');
+    });
   }
 
-  logger.info('Conectado a MongoDB');
-  app.listen(port, () => logger.info(`Escuchando en el puerto ${port}`));
-});
+  process.exit(1);
+};
+
+const unexpectedErrorHandler = (err) => {
+  logger.error(err);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
